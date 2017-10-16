773a0a5563143a1cf6cbee3fe457ce6657167605
yorek <info@davidemauri.it>
2017-10-15 17:26:27 -0700

    performance improved a lot

--- TransitiveClosureAggregatorLibrary/TransitiveClosureAggregate.cs
+++ TransitiveClosureAggregatorLibrary/TransitiveClosureAggregate.cs
@@ -33,6 +33,8 @@ namespace TransitiveClosure
 
     public class Group: IEnumerable<int>
     {
+        private int? _groupRoot = null;
+
         private Dictionary<int, bool> _group = new Dictionary<int, bool>();
 
         public Dictionary<int, bool>.KeyCollection Elements => _group.Keys;
@@ -46,12 +48,14 @@ namespace TransitiveClosure
 
         public void AddUnique(Pair pair)
         {
+            if (_groupRoot == null) _groupRoot = pair.X; 
             this.AddUnique(pair.X);
             this.AddUnique(pair.Y);
         }
 
         public void Add(int element)
         {
+            if (_groupRoot == null) _groupRoot = element;
             _group.Add(element, true);
         }
 
@@ -83,33 +87,47 @@ namespace TransitiveClosure
         {
             return (IEnumerator)GetEnumerator();
         }
+
+        public override string ToString()
+        {
+            return string.Format($"[{_groupRoot.Value}]");
+        }
     }
 
     public class GroupSet: IEnumerable<Group>
     {
         private List<Group> _groupSet = new List<Group>();
+        private Dictionary<int, Group> _numbers = new Dictionary<int, Group>();
+        private int _earlyQuit = 0;
+        private int _merges = 0;
 
-        public int Count => _groupSet.Count;
+        public int Groups => _groupSet.Count;
+
+        public int Numbers => _numbers.Count;
+
+        public int Merges => _merges;
 
         public void Add(Group group)
         {
             _groupSet.Add(group);
+
+            foreach(int e in group)
+            {
+                if (!_numbers.ContainsKey(e))
+                    _numbers.Add(e, group);
+                else
+                    throw new ApplicationException("Element is already assigned to a group");
+            }
         }
 
         public List<Group> FindInGroups(Pair p)
         {
             var result = new List<Group>();
+            
+            if (_numbers.ContainsKey(p.X)) result.Add(_numbers[p.X]);
+            if (_numbers.ContainsKey(p.Y)) result.Add(_numbers[p.Y]);
 
-            foreach (Group g in _groupSet)
-            {
-                if (g.ContainsElement(p.X) || (g.ContainsElement(p.Y)))
-                {
-                    result.Add(g);
-                    if (result.Count >= 2) break;
-                }
-            }
-
-            return result;
+            return result;                        
         }
 
         public void AddPair(Pair p)
@@ -117,7 +135,7 @@ namespace TransitiveClosure
             //Find if the inputValue is already in a group
             var foundInGroups = FindInGroups(p);
 
-            // no item matches: create a new group and add both the inputValues to it
+            // no item matches: create a new group and add both the values to it            
             if (foundInGroups.Count == 0)
             {
                 var ng = new Group();
@@ -125,6 +143,9 @@ namespace TransitiveClosure
                 ng.AddUnique(p.Y);
 
                 _groupSet.Add(ng);
+
+                if (!_numbers.ContainsKey(p.X)) _numbers.Add(p.X, ng);
+                if (!_numbers.ContainsKey(p.Y)) _numbers.Add(p.Y, ng);
             }
 
             // one item match, add the related item to the same group
@@ -132,19 +153,41 @@ namespace TransitiveClosure
             {
                 var g = foundInGroups[0];
                 g.AddUnique(p);
+
+                if (!_numbers.ContainsKey(p.X)) _numbers.Add(p.X, g); else _numbers[p.X] = g;
+                if (!_numbers.ContainsKey(p.Y)) _numbers.Add(p.Y, g); else _numbers[p.Y] = g;
             }
 
             // if there is a match for both items but in two different groups
             // merge them into just one group and delete the other
-            if (foundInGroups.Count >= 2)
+            if (foundInGroups.Count == 2)
             {
                 var g1 = foundInGroups[0];
-                for (int i = 1; i < foundInGroups.Count; i++)
+                var g2 = foundInGroups[1];
+
+                if (g1 == g2) return;
+
+                // Always move the smaller group
+                if (g2.Count > g1.Count)
                 {
-                    var g2 = foundInGroups[i];
-                    g1.MergeWith(g2);
-                    _groupSet.Remove(g2);
+                    var t = g1;
+                    g1 = g2;
+                    g2 = t;
                 }
+
+                if (!_numbers.ContainsKey(p.X)) _numbers.Add(p.X, g1); else _numbers[p.X] = g1;
+                if (!_numbers.ContainsKey(p.Y)) _numbers.Add(p.Y, g1); else _numbers[p.Y] = g1;
+
+                g1.MergeWith(g2);              
+
+                foreach(var e in g2)
+                {
+                    if (!_numbers.ContainsKey(e)) _numbers.Add(e, g1); else _numbers[e] = g1;
+                }
+
+                _merges += 1;
+
+                _groupSet.Remove(g2);
             }
         }
 
@@ -168,7 +211,11 @@ namespace TransitiveClosure
     {
         private GroupSet _groupSet;
 
-        public int Groups => _groupSet.Count;
+        public int Groups => _groupSet.Groups;
+
+        public int Numbers => _groupSet.Numbers;
+
+        public int Merges => _groupSet.Merges;
 
         public void Init()
         {
@@ -258,7 +305,7 @@ namespace TransitiveClosure
             if (w == null) throw new ArgumentNullException("w");
 
             // Group count
-            w.Write(_groupSet.Count);
+            w.Write(_groupSet.Groups);
 
             foreach (var g in _groupSet)
             {
